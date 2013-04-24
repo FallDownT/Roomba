fbdaedbaf0a17ac688a2b474dae04f9d21c3d9f6
Kevin Chapman <kchapmann@gmail.com>
2013-04-23 22:39:44 -0400

    initial implementation of bluecove api. still need to find btpp

--- src/com/github/FallDownT/Roomba/BlueCoveComm.java
+++ src/com/github/FallDownT/Roomba/BlueCoveComm.java
@@ -1,5 +1,10 @@
 package com.github.FallDownT.Roomba;
 
+import javax.microedition.io.Connector;
+import javax.microedition.io.StreamConnection;
+import java.io.IOException;
+import java.io.PrintStream;
+
 /**
  * Created with IntelliJ IDEA.
  * User: kevin
@@ -8,4 +13,20 @@ package com.github.FallDownT.Roomba;
  * To change this template use File | Settings | File Templates.
  */
 public class BlueCoveComm {
+    public static void main(String args[]) {
+
+        try {
+
+            StreamConnection conn = (StreamConnection) Connector.open("btspp://need_this:1");
+
+            PrintStream out = new PrintStream(
+                    conn.openOutputStream());
+
+            out.println("A1B1");
+
+            conn.close();
+        } catch (IOException e) {
+            System.err.print(e.toString());
+        }
+    }
 }
