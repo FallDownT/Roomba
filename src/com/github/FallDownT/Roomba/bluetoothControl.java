b553c58988ebf09987b71f989d48f1a1893c9211
Chris Schwarz <FallDownT@yahoo.com>
2013-04-25 12:42:13 -0400

    Restructured driving mechanisms to recalculate and retransmit after any change for less jerkiness.

--- src/com/github/FallDownT/Roomba/bluetoothControl.java
+++ src/com/github/FallDownT/Roomba/bluetoothControl.java
@@ -18,7 +18,7 @@ public class bluetoothControl implements BluetoothObserver {
     private PrintStream transmit;
     private BufferedReader receive;
     private boolean connected;
-    private double motor1dir, motor2dir;
+    private double motor1dir, motor2dir, leftStickMag, rightStickMag;
     private int motorBase, motor1Offset, motor2Offset, motor1Mag, motor2Mag;
     private String motor1, motor2;
     private RoombaView view;
@@ -30,12 +30,12 @@ public class bluetoothControl implements BluetoothObserver {
      * @throws IOException Bad IO.
      */
     public bluetoothControl(RoombaView v) throws IOException {
-        StreamConnection conn = (StreamConnection) Connector.open("btspp://00A09618B2D3:1");    // ECE5
-//        StreamConnection conn = (StreamConnection) Connector.open("btspp://00a0961009d2:1");    // Roomba
+//        StreamConnection conn = (StreamConnection) Connector.open("btspp://00A09618B2D3:1");    // ECE5
+        StreamConnection conn = (StreamConnection) Connector.open("btspp://00a0961009d2:1");    // Roomba
 
         transmit = new PrintStream(conn.openOutputStream());
         receive = new BufferedReader(new InputStreamReader(conn.openInputStream()));
-        motor1dir = motor2dir = 0;
+        motor1dir = motor2dir = leftStickMag = rightStickMag = 0;
         motor1 = motor2 = null;
         motorBase = motor1Offset = motor2Offset = motor1Mag = motor2Mag = 0;
         (new Thread(new BluetoothReader(receive, this))).start();   // Start the BluetoothReader thread
@@ -94,124 +94,29 @@ public class bluetoothControl implements BluetoothObserver {
 
             public void leftThumbDirection(double direction) {
                 motor1dir = direction;
+                recalcSpeed();
+                recalcTurn();
+                transmitToMotors();
             }
 
             public void rightThumbDirection(double direction) {
                 motor2dir = direction;
+                recalcSpeed();
+                recalcTurn();
+                transmitToMotors();
             }
 
             public void leftThumbMagnitude(double magnitude) {
-
-                if (motor1dir < 60 || motor1dir > 300) {    // Move forward
-                    if (magnitude > 0.80) {
-                        motorBase = 9;
-                        view.setArrowLight("up", true);
-                        view.setArrowLight("down", false);
-                    } else if (magnitude > 0.70) {
-                        motorBase = 8;
-                        view.setArrowLight("up", true);
-                        view.setArrowLight("down", false);
-                    } else if (magnitude > 0.60) {
-                        motorBase = 7;
-                        view.setArrowLight("up", true);
-                        view.setArrowLight("down", false);
-                    } else if (magnitude > 0.40) {
-                        motorBase = 6;
-                        view.setArrowLight("up", true);
-                        view.setArrowLight("down", false);
-                    } else if (magnitude > 0.20) {
-                        motorBase = 5;
-                        view.setArrowLight("up", true);
-                        view.setArrowLight("down", false);
-                    } else if (magnitude <= 0.20) {
-                        motorBase = 4;
-                        view.setArrowLight("up", false);
-                        view.setArrowLight("down", false);
-                    }
-                } else if (motor1dir < 240 || motor1dir > 120) {  // Move backward
-                    if (magnitude > 0.80) {
-                        motorBase = 0;
-                        view.setArrowLight("up", false);
-                        view.setArrowLight("down", true);
-                    } else if (magnitude > 0.60) {
-                        motorBase = 1;
-                        view.setArrowLight("up", false);
-                        view.setArrowLight("down", true);
-                    } else if (magnitude > 0.40) {
-                        motorBase = 2;
-                        view.setArrowLight("up", false);
-                        view.setArrowLight("down", true);
-                    } else if (magnitude > 0.20) {
-                        motorBase = 3;
-                        view.setArrowLight("up", false);
-                        view.setArrowLight("down", true);
-                    } else if (magnitude <= 0.20) {
-                        motorBase = 4;
-                        view.setArrowLight("up", false);
-                        view.setArrowLight("down", false);
-                    }
-
-                }
+                leftStickMag = magnitude;
+                recalcSpeed();
+                recalcTurn();
                 transmitToMotors();
             }
 
             public void rightThumbMagnitude(double magnitude) {
-
-                if (motor2dir < 330 || motor2dir > 210) {    // Turn left
-                    if (magnitude > 0.80) {
-                        motor1Offset = -2;
-                        motor2Offset = 2;
-                        view.setArrowLight("left", false);
-                        view.setArrowLight("right", true);
-                    } else if (magnitude > 0.60) {
-                        motor1Offset = -1;
-                        motor2Offset = 2;
-                        view.setArrowLight("left", false);
-                        view.setArrowLight("right", true);
-                    } else if (magnitude > 0.40) {
-                        motor1Offset = -1;
-                        motor2Offset = 1;
-                        view.setArrowLight("left", false);
-                        view.setArrowLight("right", true);
-                    } else if (magnitude > 0.20) {
-                        motor1Offset = 0;
-                        motor2Offset = 1;
-                        view.setArrowLight("left", false);
-                        view.setArrowLight("right", true);
-                    } else if (magnitude <= 0.20) {
-                        motor1Offset = 0;
-                        motor2Offset = 0;
-                        view.setArrowLight("left", false);
-                        view.setArrowLight("right", false);
-                    }
-                } else if (motor2dir < 150 || motor2dir > 30) {  // Turn right
-                    if (magnitude > 0.80) {
-                        motor1Offset = 2;
-                        motor2Offset = -2;
-                        view.setArrowLight("left", true);
-                        view.setArrowLight("right", false);
-                    } else if (magnitude > 0.60) {
-                        motor1Offset = 2;
-                        motor2Offset = -1;
-                        view.setArrowLight("left", true);
-                        view.setArrowLight("right", false);
-                    } else if (magnitude > 0.40) {
-                        motor1Offset = 1;
-                        motor2Offset = -1;
-                        view.setArrowLight("left", true);
-                        view.setArrowLight("right", false);
-                    } else if (magnitude > 0.20) {
-                        motor1Offset = 1;
-                        motor2Offset = 0;
-                        view.setArrowLight("left", true);
-                        view.setArrowLight("right", false);
-                    } else if (magnitude <= 0.20) {
-                        motor1Offset = 0;
-                        motor2Offset = 0;
-                        view.setArrowLight("left", false);
-                        view.setArrowLight("right", false);
-                    }
-                }
+                rightStickMag = magnitude;
+                recalcSpeed();
+                recalcTurn();
                 transmitToMotors();
             }
 
@@ -246,6 +151,122 @@ public class bluetoothControl implements BluetoothObserver {
             }
 
             public void dpad(int direction, boolean pressed) {
+            }
+
+            /**
+             * Recalculate the motor speed.
+             */
+            private void recalcSpeed() {
+                if (motor1dir < 60 || motor1dir > 300) {    // Move forward
+                    if (leftStickMag > 0.80) {
+                        motorBase = 9;
+                        view.setArrowLight("up", true);
+                        view.setArrowLight("down", false);
+                    } else if (leftStickMag > 0.70) {
+                        motorBase = 8;
+                        view.setArrowLight("up", true);
+                        view.setArrowLight("down", false);
+                    } else if (leftStickMag > 0.60) {
+                        motorBase = 7;
+                        view.setArrowLight("up", true);
+                        view.setArrowLight("down", false);
+                    } else if (leftStickMag > 0.40) {
+                        motorBase = 6;
+                        view.setArrowLight("up", true);
+                        view.setArrowLight("down", false);
+                    } else if (leftStickMag > 0.20) {
+                        motorBase = 5;
+                        view.setArrowLight("up", true);
+                        view.setArrowLight("down", false);
+                    } else if (leftStickMag <= 0.20) {
+                        motorBase = 4;
+                        view.setArrowLight("up", false);
+                        view.setArrowLight("down", false);
+                    }
+                } else if (motor1dir < 240 || motor1dir > 120) {  // Move backward
+                    if (leftStickMag > 0.80) {
+                        motorBase = 0;
+                        view.setArrowLight("up", false);
+                        view.setArrowLight("down", true);
+                    } else if (leftStickMag > 0.60) {
+                        motorBase = 1;
+                        view.setArrowLight("up", false);
+                        view.setArrowLight("down", true);
+                    } else if (leftStickMag > 0.40) {
+                        motorBase = 2;
+                        view.setArrowLight("up", false);
+                        view.setArrowLight("down", true);
+                    } else if (leftStickMag > 0.20) {
+                        motorBase = 3;
+                        view.setArrowLight("up", false);
+                        view.setArrowLight("down", true);
+                    } else if (leftStickMag <= 0.20) {
+                        motorBase = 4;
+                        view.setArrowLight("up", false);
+                        view.setArrowLight("down", false);
+                    }
+                }
+            }
+
+            /**
+             * Recalculate the motor offsets
+             */
+            private void recalcTurn() {
+                if (motor2dir < 330 || motor2dir > 210) {    // Turn left
+                    if (rightStickMag > 0.80) {
+                        motor1Offset = -2;
+                        motor2Offset = 2;
+                        view.setArrowLight("left", false);
+                        view.setArrowLight("right", true);
+                    } else if (rightStickMag > 0.60) {
+                        motor1Offset = -1;
+                        motor2Offset = 2;
+                        view.setArrowLight("left", false);
+                        view.setArrowLight("right", true);
+                    } else if (rightStickMag > 0.40) {
+                        motor1Offset = -1;
+                        motor2Offset = 1;
+                        view.setArrowLight("left", false);
+                        view.setArrowLight("right", true);
+                    } else if (rightStickMag > 0.20) {
+                        motor1Offset = 0;
+                        motor2Offset = 1;
+                        view.setArrowLight("left", false);
+                        view.setArrowLight("right", true);
+                    } else if (rightStickMag <= 0.20) {
+                        motor1Offset = 0;
+                        motor2Offset = 0;
+                        view.setArrowLight("left", false);
+                        view.setArrowLight("right", false);
+                    }
+                } else if (motor2dir < 150 || motor2dir > 30) {  // Turn right
+                    if (rightStickMag > 0.80) {
+                        motor1Offset = 2;
+                        motor2Offset = -2;
+                        view.setArrowLight("left", true);
+                        view.setArrowLight("right", false);
+                    } else if (rightStickMag > 0.60) {
+                        motor1Offset = 2;
+                        motor2Offset = -1;
+                        view.setArrowLight("left", true);
+                        view.setArrowLight("right", false);
+                    } else if (rightStickMag > 0.40) {
+                        motor1Offset = 1;
+                        motor2Offset = -1;
+                        view.setArrowLight("left", true);
+                        view.setArrowLight("right", false);
+                    } else if (rightStickMag > 0.20) {
+                        motor1Offset = 1;
+                        motor2Offset = 0;
+                        view.setArrowLight("left", true);
+                        view.setArrowLight("right", false);
+                    } else if (rightStickMag <= 0.20) {
+                        motor1Offset = 0;
+                        motor2Offset = 0;
+                        view.setArrowLight("left", false);
+                        view.setArrowLight("right", false);
+                    }
+                }
             }
 
             /**
