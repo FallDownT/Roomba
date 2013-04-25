c0d17bfe614fd7125fa5196a89ba99b9f7d972fe
Chris Schwarz <FallDownT@yahoo.com>
2013-04-25 12:28:29 -0400

    Rearranged the user driving interface to be similar to that of Halo.

--- src/com/github/FallDownT/Roomba/bluetoothControl.java
+++ src/com/github/FallDownT/Roomba/bluetoothControl.java
@@ -19,6 +19,7 @@ public class bluetoothControl implements BluetoothObserver {
     private BufferedReader receive;
     private boolean connected;
     private double motor1dir, motor2dir;
+    private int motorBase, motor1Offset, motor2Offset, motor1Mag, motor2Mag;
     private String motor1, motor2;
     private RoombaView view;
 
@@ -29,15 +30,14 @@ public class bluetoothControl implements BluetoothObserver {
      * @throws IOException Bad IO.
      */
     public bluetoothControl(RoombaView v) throws IOException {
-        StreamConnection conn = (StreamConnection) Connector.open("btspp://00A09618B2D3:1");          //ece5
-//        StreamConnection conn = (StreamConnection) Connector.open("btspp://00a0961009d2:1");         //roomba
+        StreamConnection conn = (StreamConnection) Connector.open("btspp://00A09618B2D3:1");    // ECE5
+//        StreamConnection conn = (StreamConnection) Connector.open("btspp://00a0961009d2:1");    // Roomba
 
         transmit = new PrintStream(conn.openOutputStream());
         receive = new BufferedReader(new InputStreamReader(conn.openInputStream()));
-        motor1dir = 0;
-        motor2dir = 0;
-        motor1 = null;
-        motor2 = null;
+        motor1dir = motor2dir = 0;
+        motor1 = motor2 = null;
+        motorBase = motor1Offset = motor2Offset = motor1Mag = motor2Mag = 0;
         (new Thread(new BluetoothReader(receive, this))).start();   // Start the BluetoothReader thread
         view = v;
     }
@@ -81,11 +81,6 @@ public class bluetoothControl implements BluetoothObserver {
             System.out.println("*Controller Connected*");
         }
 
-
-        view.getDrawingPanel().setPoint(15, 150, 288);
-        view.getDrawingPanel().setPoint(31, 310, 100);
-        view.getDrawingPanel().setPoint(14, 140, 260);
-
         controller.addXboxControllerListener(new XboxControllerAdapter() {
             public void isConnected(boolean b) {
                 connected = b;
@@ -107,92 +102,117 @@ public class bluetoothControl implements BluetoothObserver {
 
             public void leftThumbMagnitude(double magnitude) {
 
-                if (motor1dir < 60 || motor1dir > 300) {    // Left wheel forward
+                if (motor1dir < 60 || motor1dir > 300) {    // Move forward
                     if (magnitude > 0.80) {
-                        motor1 = "A9";
+                        motorBase = 9;
                         view.setArrowLight("up", true);
+                        view.setArrowLight("down", false);
                     } else if (magnitude > 0.70) {
-                        motor1 = "A8";
+                        motorBase = 8;
                         view.setArrowLight("up", true);
+                        view.setArrowLight("down", false);
                     } else if (magnitude > 0.60) {
-                        motor1 = "A7";
+                        motorBase = 7;
                         view.setArrowLight("up", true);
+                        view.setArrowLight("down", false);
                     } else if (magnitude > 0.40) {
-                        motor1 = "A6";
+                        motorBase = 6;
                         view.setArrowLight("up", true);
+                        view.setArrowLight("down", false);
                     } else if (magnitude > 0.20) {
-                        motor1 = "A5";
+                        motorBase = 5;
                         view.setArrowLight("up", true);
+                        view.setArrowLight("down", false);
                     } else if (magnitude <= 0.20) {
-                        motor1 = "A4";
+                        motorBase = 4;
                         view.setArrowLight("up", false);
+                        view.setArrowLight("down", false);
                     }
-                } else if (motor1dir < 240 || motor1dir > 120) {  // Left wheel backward
+                } else if (motor1dir < 240 || motor1dir > 120) {  // Move backward
                     if (magnitude > 0.80) {
-                        motor1 = "A0";
+                        motorBase = 0;
+                        view.setArrowLight("up", false);
                         view.setArrowLight("down", true);
                     } else if (magnitude > 0.60) {
-                        motor1 = "A1";
+                        motorBase = 1;
+                        view.setArrowLight("up", false);
                         view.setArrowLight("down", true);
                     } else if (magnitude > 0.40) {
-                        motor1 = "A2";
+                        motorBase = 2;
+                        view.setArrowLight("up", false);
                         view.setArrowLight("down", true);
                     } else if (magnitude > 0.20) {
-                        motor1 = "A3";
+                        motorBase = 3;
+                        view.setArrowLight("up", false);
                         view.setArrowLight("down", true);
                     } else if (magnitude <= 0.20) {
-                        motor1 = "A4";
+                        motorBase = 4;
+                        view.setArrowLight("up", false);
                         view.setArrowLight("down", false);
                     }
 
                 }
-
-                transmit.println(motor1);
+                transmitToMotors();
             }
 
             public void rightThumbMagnitude(double magnitude) {
 
-                if (motor2dir < 60 || motor2dir > 300) {    // Right wheel moving forward
+                if (motor2dir < 330 || motor2dir > 210) {    // Turn left
                     if (magnitude > 0.80) {
-                        motor2 = "B9";
-                        view.setArrowLight("right", true);
-                    } else if (magnitude > 0.70) {
-                        motor2 = "B8";
+                        motor1Offset = -2;
+                        motor2Offset = 2;
+                        view.setArrowLight("left", false);
                         view.setArrowLight("right", true);
                     } else if (magnitude > 0.60) {
-                        motor2 = "B7";
+                        motor1Offset = -1;
+                        motor2Offset = 2;
+                        view.setArrowLight("left", false);
                         view.setArrowLight("right", true);
                     } else if (magnitude > 0.40) {
-                        motor2 = "B6";
+                        motor1Offset = -1;
+                        motor2Offset = 1;
+                        view.setArrowLight("left", false);
                         view.setArrowLight("right", true);
                     } else if (magnitude > 0.20) {
-                        motor2 = "B5";
+                        motor1Offset = 0;
+                        motor2Offset = 1;
+                        view.setArrowLight("left", false);
                         view.setArrowLight("right", true);
                     } else if (magnitude <= 0.20) {
-                        motor2 = "B4";
+                        motor1Offset = 0;
+                        motor2Offset = 0;
+                        view.setArrowLight("left", false);
                         view.setArrowLight("right", false);
                     }
-                } else if (motor2dir < 240 || motor2dir > 120) {  // Right wheel backward
+                } else if (motor2dir < 150 || motor2dir > 30) {  // Turn right
                     if (magnitude > 0.80) {
-                        motor2 = "B0";
+                        motor1Offset = 2;
+                        motor2Offset = -2;
                         view.setArrowLight("left", true);
+                        view.setArrowLight("right", false);
                     } else if (magnitude > 0.60) {
-                        motor2 = "B1";
+                        motor1Offset = 2;
+                        motor2Offset = -1;
                         view.setArrowLight("left", true);
+                        view.setArrowLight("right", false);
                     } else if (magnitude > 0.40) {
-                        motor2 = "B2";
+                        motor1Offset = 1;
+                        motor2Offset = -1;
                         view.setArrowLight("left", true);
+                        view.setArrowLight("right", false);
                     } else if (magnitude > 0.20) {
-                        motor2 = "B3";
+                        motor1Offset = 1;
+                        motor2Offset = 0;
                         view.setArrowLight("left", true);
+                        view.setArrowLight("right", false);
                     } else if (magnitude <= 0.20) {
-                        motor2 = "B4";
+                        motor1Offset = 0;
+                        motor2Offset = 0;
                         view.setArrowLight("left", false);
+                        view.setArrowLight("right", false);
                     }
-
                 }
-
-                transmit.println(motor2);
+                transmitToMotors();
             }
 
             public void buttonA(boolean pressed) {
@@ -226,6 +246,37 @@ public class bluetoothControl implements BluetoothObserver {
             }
 
             public void dpad(int direction, boolean pressed) {
+            }
+
+            /**
+             * Calculate motor magnitudes and transmit to motors.
+             */
+            private void transmitToMotors() {
+                // Calculate motor magnitudes based on base speed and offset
+                motor1Mag = motorBase + motor1Offset;
+                motor2Mag = motorBase + motor2Offset;
+
+                // Adjust motor magnitudes to be within acceptable range
+                while (motor1Mag < 0) {  // Increment motor1Mag to a value above 0
+                    motor1Mag++;
+                }
+                while (motor1Mag > 9) {  // Increment motor1Mag to a value above 0
+                    motor1Mag--;
+                }
+                while (motor2Mag < 0) {  // Increment motor2Mag to a value below 9
+                    motor2Mag++;
+                }
+                while (motor2Mag > 9) {  // Increment motor2Mag to a value below 9
+                    motor2Mag--;
+                }
+
+                // Assign Strings for Bluetooth transmission
+                motor1 = "A" + motor1Mag;
+                motor2 = "B" + motor2Mag;
+
+                // Transmit motor adjustment commands via Bluetooth
+                transmit.println(motor1);
+                transmit.println(motor2);
             }
         });
 
